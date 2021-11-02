290a88a7dbecf25f86bf3e38453f9f23ab1742af
Shoko <shsuzu1992@gmail.com>
2021-11-03 00:00:28 +0900

    Initial Commit

---
+++ Kadai-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadai-2
+//
+//  Created by 鈴木淳子 on 2021/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
